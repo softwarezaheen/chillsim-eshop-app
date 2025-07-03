676bdea1b7378a85fa3348664e7d2063cfdc271d
Mahdi Rida <mahdi.rida@montymobile.com>
2025-07-03 10:09:41 +0300

    Opt
    Add facebook keys
    add facebook scheme
    add chillsim base url
    upgrade supabase version

--- lib/app/environment/envs/env_open_source_dev.dart
+++ lib/app/environment/envs/env_open_source_dev.dart
@@ -1,7 +1,8 @@
 import "package:esim_open_source/app/environment/app_environment_helper.dart";
 
 AppEnvironmentHelper openSourceDebugEnvInstance = AppEnvironmentHelper(
-  baseApiUrl: "",
+  baseApiUrl: "https://chillsim-eshop-api.onrender.com",
+  websiteUrl: "https://chillsim-eshop-web.onrender.com",
   omniConfigBaseUrl: "",
   omniConfigTenant: "",
   omniConfigApiKey: "",
--- lib/app/environment/envs/env_open_source_release.dart
+++ lib/app/environment/envs/env_open_source_release.dart
@@ -1,7 +1,8 @@
 import "package:esim_open_source/app/environment/app_environment_helper.dart";
 
 AppEnvironmentHelper openSourceProdEnvInstance = AppEnvironmentHelper(
-  baseApiUrl: "",
+  baseApiUrl: "https://chillsim-eshop-api.onrender.com",
+  websiteUrl: "https://chillsim-eshop-web.onrender.com",
   omniConfigBaseUrl: "",
   omniConfigTenant: "",
   omniConfigApiKey: "",
