9af1f477baa4a2fa6bdbbc1a0802eefd5bb71f55
Mahdi Rida <mahdi.rida@montymobile.com>
2025-07-28 16:55:39 +0300

    Release version
    upload android version to play store

--- lib/app/environment/envs/env_open_source_staging.dart
+++ lib/app/environment/envs/env_open_source_staging.dart
@@ -3,9 +3,9 @@ import "package:esim_open_source/app/environment/app_environment_helper.dart";
 AppEnvironmentHelper openSourceStagingEnvInstance = AppEnvironmentHelper(
   baseApiUrl: "https://chillsim-eshop-api.onrender.com",
   websiteUrl: "chillsim-eshop-web.onrender.com",
-  enableBannersView: true,
-  enableWalletView: true,
-  enablePromoCode: true,
+  enableBannersView: false,
+  enableWalletView: false,
+  enablePromoCode: false,
   omniConfigBaseUrl: "",
   omniConfigTenant: "",
   omniConfigApiKey: "",
