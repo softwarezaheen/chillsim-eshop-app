28ccdef3cb2ec31b00968ae592f8b8594b061caa
Mahdi Rida <mahdi.rida@montymobile.com>
2025-08-28 15:40:58 +0300

    merge with main

--- lib/translations/locale_keys.g.dart
+++ lib/translations/locale_keys.g.dart
@@ -1,6 +1,6 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-// ignore_for_file: type=lint
+// ignore_for_file: constant_identifier_names
 
 abstract class  LocaleKeys {
   static const appName = 'appName';
